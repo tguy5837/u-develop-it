3d6eca8fde3052d6ab5305477cd37269cd7332d7
DTGuy <tguy5837@gmail.com>
2022-10-10 22:39:34 -0400

    added mysql database to server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,7 +7,18 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
